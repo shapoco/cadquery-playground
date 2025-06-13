73126f9a4ce447b3572b411c47901549282f4126
shapoco <40152459+shapoco@users.noreply.github.com>
2025-06-13 23:42:58 +0900

    refactoring

--- mandelbrot_sauce_plate/mandelbrot_sauce_plate.cq.py
+++ mandelbrot_sauce_plate/mandelbrot_sauce_plate.cq.py
@@ -1,30 +1,45 @@
 import cadquery as cq
 import numpy as np
 
-SCALE = 40
-Z_SCALE = 20
-BASE_R = 30
-#PITCH_GOAL = 2
+# 皿の床面の半径
+FLOOR_R = 30
+
+# 皿の底面から彫り込みの底までの厚み
+BOTTOM_H = 3
+
+# 彫り込みの深さ
+IMAGE_DEPTH = 5
+
+# 皿の底の高さ
+FLOOR_H = BOTTOM_H + IMAGE_DEPTH
+
+# マンデルブロ集合のピクセルサイズの目標値
 PITCH_GOAL = 0.4
-BASE_R2 = BASE_R - PITCH_GOAL * 2
-N_DIV = int(np.ceil(BASE_R2 * 2 / PITCH_GOAL))
-PITCH = BASE_R2 * 2 / N_DIV
-BASE_L = 3
-ENG_DEPTH = 5
-BASE_H = BASE_L + ENG_DEPTH
-DEPTH = 5
-THICKNESS = 5
 
-AC = -0.5
-BC = 0
-AB_R = 1.5
+# マンデルブロ集合描画領域の半径
+# (底の半径より少し狭くする)
+IMAGE_R = FLOOR_R - PITCH_GOAL * 2
 
-A0 = AC - AB_R
-A1 = AC + AB_R
-B0 = BC - AB_R
-B1 = BC + AB_R
+# マンデルブロ集合の解像度
+NUM_PIXELS = int(np.ceil(IMAGE_R * 2 / PITCH_GOAL))
 
+# マンデルブロ集合のピクセルサイズの実際値
+PITCH = IMAGE_R * 2 / NUM_PIXELS
 
+# 皿のフチの高さ
+WALL_H = 5
+
+# 皿のフチの水平方向の厚み
+WALL_T = 5
+
+# マンデルブロ集合の注目座標
+RENDER_REAL = -0.5
+RENDER_IMAG = 0
+
+# マンデルブロ集合の描画半径
+RENDER_R = 1.5
+
+# マンデルブロ集合関数
 def mandelbrot(a, b, max_iter):
     z = 0
     c = complex(a, b)
@@ -34,40 +49,51 @@ def mandelbrot(a, b, max_iter):
         z = z * z + c
     return max_iter
 
-
+# 彫刻用の cutter 生成
 def build(ix0, ix1):
-    ixc = (ix0 + ix1) // 2
-    x0 = -BASE_R2 + BASE_R2 * 2 * ix0 / N_DIV
-    x1 = -BASE_R2 + BASE_R2 * 2 * ix1 / N_DIV
+    # 描画範囲の算出
+    real0 = RENDER_REAL - RENDER_R
+    real1 = RENDER_REAL + RENDER_R
+    imag0 = RENDER_IMAG - RENDER_R
+    imag1 = RENDER_IMAG + RENDER_R
+
+    x0 = -IMAGE_R + IMAGE_R * 2 * ix0 / NUM_PIXELS
+    x1 = -IMAGE_R + IMAGE_R * 2 * ix1 / NUM_PIXELS
     x_thick = x1 - x0
     x_middle = (x0 + x1) / 2
-    a = AC + AB_R * x_middle / BASE_R2
+    a = RENDER_REAL + RENDER_R * x_middle / IMAGE_R
 
     if ix0 + 1 == ix1:
-        r = BASE_R2 - PITCH / 2
+        # 縦一列分の描画
+        
+        # 縦方向の描画範囲の算出
+        # (丸い皿の底から飛び出さないように)
+        r = IMAGE_R - PITCH / 2
         iy0 = int(np.floor(-np.sqrt(r**2 - x_middle**2) / PITCH)) - 1
         iy1 = -iy0
-        iy0 += int(N_DIV // 2)
-        iy1 += int(N_DIV // 2)
+        iy0 += int(NUM_PIXELS // 2)
+        iy1 += int(NUM_PIXELS // 2)
+        y0 = -IMAGE_R + IMAGE_R * 2 * iy0 / NUM_PIXELS
+        y1 = -IMAGE_R + IMAGE_R * 2 * iy1 / NUM_PIXELS
 
-        y0 = -BASE_R2 + BASE_R2 * 2 * iy0 / N_DIV
-        y1 = -BASE_R2 + BASE_R2 * 2 * iy1 / N_DIV
-
+        # 断面座標の生成
         max_iter = 100
         z_last = -1
         verts = [(y0, 5)]
         for iy in range(iy0, iy1):
-            y = -BASE_R2 + BASE_R2 * 2 * iy / N_DIV
-            b = BC + AB_R * (y + PITCH / 2) / BASE_R2
+            y = -IMAGE_R + IMAGE_R * 2 * iy / NUM_PIXELS
+            b = RENDER_IMAG + RENDER_R * (y + PITCH / 2) / IMAGE_R
             z = (mandelbrot(a, b, max_iter)) / max_iter
             z = 1 - (1 - z) ** 4
-            z = -ENG_DEPTH * z
+            z = -IMAGE_DEPTH * z
             if z != z_last:
                 verts.append((y, z_last))
                 verts.append((y, z))
             z_last = z
         verts.append((y1, z_last))
         verts.append((y1, 5))
+        
+        # 押し出しで断面を生成
         return (
             cq.Workplane("YZ")
             .polyline(verts)
@@ -76,23 +102,24 @@ def build(ix0, ix1):
             .translate((x0, 0, 0))
         )
     else:
+        # 分割-->統合
+        ixc = (ix0 + ix1) // 2
         sub0 = build(ix0, ixc)
         sub1 = build(ixc, ix1)
         return sub0.union(sub1)
 
-
-dish_inner_verts = [
+# 皿の生成
+dish_verts = [
     (0, 0),
-    (0, BASE_H),
-    (BASE_R, BASE_H),
-    (BASE_R + DEPTH * 1.5, BASE_H + DEPTH),
-    (BASE_R + DEPTH * 1.5 + THICKNESS, BASE_H + DEPTH),
-    (BASE_R + DEPTH * 0.5 - BASE_H, 0),
+    (0, FLOOR_H),
+    (FLOOR_R, FLOOR_H),
+    (FLOOR_R + WALL_H * 1.5, FLOOR_H + WALL_H),
+    (FLOOR_R + WALL_H * 1.5 + WALL_T, FLOOR_H + WALL_H),
+    (FLOOR_R + WALL_H * 0.5 - FLOOR_H, 0),
 ]
-
-dish_inner = (
+dish = (
     cq.Workplane("YZ")
-    .polyline(dish_inner_verts)
+    .polyline(dish_verts)
     .close()
     .revolve(360, (0, 0, 0), (0, 1, 0))
     .faces(">Z")
@@ -101,11 +128,14 @@ dish_inner = (
     .fillet(5)
 )
 
-cutter = build(0, N_DIV).translate((0, 0, BASE_H + 0.4))
+# マンデルブロ集合を彫り込むための cutter を生成
+cutter = build(0, NUM_PIXELS).translate((0, 0, FLOOR_H + 0.4))
 
 #show_object(cutter)
 
-result = dish_inner.cut(cutter)
+# 彫り込み
+dish = dish.cut(cutter)
 
-show_object(result)
-result.export("mandelbrot.step")
+# 出力
+show_object(dish)
+dish.export("mandelbrot_sauce_plate.step")
