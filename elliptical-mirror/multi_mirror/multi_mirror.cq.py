f6328745a0a466842a3934c7ed6bdb676a68a45f
shapoco <40152459+shapoco@users.noreply.github.com>
2025-06-29 22:51:11 +0900

    split mirror into reflector and frame

--- elliptical-mirror/multi_mirror/multi_mirror.cq.py
+++ elliptical-mirror/multi_mirror/multi_mirror.cq.py
@@ -121,26 +121,31 @@ def intersection_plane_line(plane_p, plane_v1, plane_v2, line_p, line_v):
 
 # ----------------------------------------------------------------------
 
+SCALING = 1.5
+
+ENABLE_CHAMFER = True
 
 # 反射板の数
 NUM_REFLECTORS = 6
 
 # 反射板の寸法 [mm]
-REFLECTOR_INNER_RADIUS = 10
-REFLECTOR_OUTER_RADIUS = 60
-REFLECTOR_HEIGHT = 65
+REFLECTOR_INNER_RADIUS = 10 * SCALING
+REFLECTOR_OUTER_RADIUS = 60 * SCALING
+REFLECTOR_HEIGHT = 65 * SCALING
+REFLECTOR_TOP_CUT_SIZE = 10 * SCALING
+REFLECTOR_THICKNESS = 3
 
 # 焦点間の距離 [mm]
-ELLIPSE_F_DISTANCE = 40
+ELLIPSE_F_DISTANCE = 40 * SCALING
 
 # 楕円の傾き [度]
 ELLIPSE_ANGLE = 45
 
 # 楕円の半径 [mm]
-ELLIPSE_R_SHORT = 40
+ELLIPSE_R_SHORT = 40 * SCALING
 ELLIPSE_R_LONG = math.sqrt((ELLIPSE_F_DISTANCE / 2) ** 2 + ELLIPSE_R_SHORT**2)
 
-ELLIPSE_FLOOR_Z = -10
+ELLIPSE_FLOOR_Z = -10 * SCALING
 
 # 焦点の座標
 ELLIPSE_F_NEAR = rotate3d(
@@ -149,32 +154,35 @@ ELLIPSE_F_NEAR = rotate3d(
 ELLIPSE_F_FAR = rotate3d((0, 0, 0), (0, 0, 0), (0, -1, 0), math.radians(ELLIPSE_ANGLE))
 
 # 嵌合用の隙間 [mm]
-GENERIC_GAP = 0.2
+GENERIC_GAP = 0.5
 
 # 面取りのサイズ [mm]
 CHAMFER = 0.5
 
 # 固定用キャップの穴の間隔 [mm]
-REF_INNER_MOUNT_X = 15
-REF_OUTER_MOUNT_X = 30
-REF_OUTER_MOUNT_Y = 10
+REF_INNER_MOUNT_X = 15 * SCALING
+REF_OUTER_MOUNT_X = 30 * SCALING
+REF_OUTER_MOUNT_Y = 10 * SCALING
 
 # 固定用キャップの直径 [mm]
-CAP_DIAMETER = (REF_OUTER_MOUNT_X + 12.5) * 2
+CAP_DIAMETER = (REF_OUTER_MOUNT_X + 12.5 * SCALING) * 2
 
 # 固定用キャップの穴の間隔 [mm]
-CAP_MOUNT_X = 15
-CAP_MOUNT_Y = 20
+# CAP_MOUNT_X = 15
+# CAP_MOUNT_Y = 20
+CAP_MOUNT_X = 20
+CAP_MOUNT_Y = 30
 
 # LED アームの取り付け用穴の間隔 [mm]
-LED_ARM_HOLE_DISTANCE = 10
+ARM_MOUNT_HOLE_DISTANCE = 10
 
-ARM_HOLDER_HEIGHT = 30
+ARM_BASE_HEIGHT = 30
+ARM_MOUNT_Z_OFFSET = 10 * SCALING
 
 STEP_OUT_DIR = "./step"
 
 
-class Reflector:
+class MirrorSegment:
     def __init__(self):
         inner_sin = REFLECTOR_INNER_RADIUS * math.sin(math.pi / NUM_REFLECTORS)
         inner_cos = REFLECTOR_INNER_RADIUS * math.cos(math.pi / NUM_REFLECTORS)
@@ -232,10 +240,10 @@ class Reflector:
 
         # 天井を斜めにする
         verts = [
-            (REFLECTOR_OUTER_RADIUS - 10, REFLECTOR_HEIGHT + 10),
+            (REFLECTOR_OUTER_RADIUS - REFLECTOR_TOP_CUT_SIZE, REFLECTOR_HEIGHT + 10),
             (REFLECTOR_OUTER_RADIUS, REFLECTOR_HEIGHT + 10),
-            (REFLECTOR_OUTER_RADIUS, REFLECTOR_HEIGHT - 10),
-            (REFLECTOR_OUTER_RADIUS - 10, REFLECTOR_HEIGHT),
+            (REFLECTOR_OUTER_RADIUS, REFLECTOR_HEIGHT - REFLECTOR_TOP_CUT_SIZE),
+            (REFLECTOR_OUTER_RADIUS - REFLECTOR_TOP_CUT_SIZE, REFLECTOR_HEIGHT),
         ]
         cutter = (
             cq.Workplane("XZ")
@@ -247,7 +255,9 @@ class Reflector:
 
         # 穴開け
         solid = (
-            solid.faces(">Z")
+            solid
+            # キャップ用の穴
+            .faces(">Z")
             .workplane(origin=(0, 0, 0))
             .pushPoints(
                 [
@@ -258,12 +268,16 @@ class Reflector:
             )
             .circle(2.5 / 2)
             .cutBlind(-5)
-            .faces(">>X[-3]")
+            # LED アーム用の穴
+            .faces(">>X[-2]")
             .workplane(origin=(0, 0, 0))
             .pushPoints(
                 [
-                    (0, REFLECTOR_HEIGHT - 10),
-                    (0, REFLECTOR_HEIGHT - 10 - LED_ARM_HOLE_DISTANCE),
+                    (0, REFLECTOR_HEIGHT - ARM_MOUNT_Z_OFFSET),
+                    (
+                        0,
+                        REFLECTOR_HEIGHT - ARM_MOUNT_Z_OFFSET - ARM_MOUNT_HOLE_DISTANCE,
+                    ),
                 ]
             )
             .circle(2.5 / 2)
@@ -271,19 +285,43 @@ class Reflector:
         )
 
         # 面取り
-        solid = (
-            solid
-            # .faces("<X or <<Y[-2] or >>Y[-2] or <<Z[-2]")
-            .faces("<X or <<Y[-2] or >>Y[-2] or <<Z[-3]")
-            .chamfer(CHAMFER)
-            .edges("%circle")
-            .chamfer(CHAMFER)
+        if ENABLE_CHAMFER:
+            solid = (
+                solid.faces("<X or <<Y[-2] or >>Y[-2] or <<Z[-2]")
+                # .faces("<X or <<Y[-2] or >>Y[-2] or <<Z[-3]")
+                .chamfer(CHAMFER)
+                .edges("%circle")
+                .chamfer(CHAMFER)
+            )
+
+        intersector = (
+            cq.Workplane("XY")
+            .ellipseArc(
+                ELLIPSE_R_LONG + REFLECTOR_THICKNESS,
+                ELLIPSE_R_SHORT + REFLECTOR_THICKNESS,
+                0,
+                180,
+                sense=-1,
+                startAtCurrent=False,
+            )
+            .close()
+            .revolve(360, (0, 0, 0), (1, 0, 0))
+            .translate((ELLIPSE_F_DISTANCE / 2, 0, 0))
+            .rotate((0, 1, 0), (0, 0, 0), ELLIPSE_ANGLE)
         )
 
-        self.solid = solid
+        # 反射板を切り出す
+        reflector = solid.intersect(intersector)
+
+        # フレームを切り出す
+        frame = solid.cut(intersector)
+
+        self.reflector_solid = reflector
+        self.frame_solid = frame
+        self.bounding_solid = solid
 
 
-class Cap:
+class MirrorFastener:
     def __init__(self):
         solid = (
             cq.Workplane("XY")
@@ -356,7 +394,7 @@ class Cap:
         self.solid = solid
 
 
-class ArmHolder:
+class LedArmBase:
     def __init__(self):
         verts = [
             (0, -10),
@@ -372,7 +410,7 @@ class ArmHolder:
             cq.Workplane("XY")
             .polyline(verts)
             .close()
-            .extrude(ARM_HOLDER_HEIGHT)
+            .extrude(ARM_BASE_HEIGHT)
             # 穴開け
             .faces(">X")
             .workplane(origin=(0, 0, 0))
@@ -392,14 +430,18 @@ class ArmHolder:
         )
 
         solid = solid.translate(
-            (REFLECTOR_OUTER_RADIUS - 5, 0, REFLECTOR_HEIGHT - ARM_HOLDER_HEIGHT - 5)
+            (
+                REFLECTOR_OUTER_RADIUS - 5,
+                0,
+                REFLECTOR_HEIGHT - ARM_BASE_HEIGHT - ARM_MOUNT_Z_OFFSET + 5,
+            )
         )
 
         self.solid = solid
 
 
 class FocusIndicator:
-    def __init__(self, ref: Reflector):
+    def __init__(self, body: MirrorSegment):
         # 内側の足の座標
         # (inner_edge_x, inner_edge_y) = rotate2d((REFLECTOR_INNER_RADIUS, 0), math.pi / NUM_REFLECTORS)
         (inner_edge_x, inner_edge_y) = (
@@ -451,6 +493,9 @@ class FocusIndicator:
             intersector_p2,
         )
         foot_pos2 = max(intersections, key=lambda p: p[2])
+        foot_pos2 = add3d(
+            foot_pos2, mul3d(norm3d(sub3d(foot_pos1, foot_pos2)), 2 * SCALING)
+        )
 
         # スケッチのため YZ 平面に回転
         sketch_angle_rad = math.atan2(
@@ -464,11 +509,11 @@ class FocusIndicator:
         near_rot = rotate3d(ELLIPSE_F_NEAR, (0, 0, 0), (0, -1, 0), -sketch_angle_rad)
         far_rot = rotate3d(ELLIPSE_F_FAR, (0, 0, 0), (0, -1, 0), -sketch_angle_rad)
 
-        wall_height = 6
+        wall_height = 10
         wall_thickness = 2
 
         # 足を形成
-        foot_radius = 5
+        foot_radius = 5 * SCALING
         foot1_pos_xy = (foot_pos1_rot[1], foot_pos1_rot[2])
         foot2_pos_xy = (foot_pos2_rot[1], foot_pos2_rot[2])
         foot3_pos_xy = (-foot_pos2_rot[1], foot_pos2_rot[2])
@@ -508,9 +553,9 @@ class FocusIndicator:
         )
 
         arrow_thickness = 1
-        arrow_size = 2
-        arrow_space_size = arrow_size + 5
-        arrow_body_size = arrow_space_size + 5
+        arrow_size = 2 * SCALING
+        arrow_space_size = arrow_size + 5 * SCALING
+        arrow_body_size = arrow_space_size + 3 * SCALING
         verts = [
             (arrow_body_size, -arrow_size),
             (arrow_space_size, -arrow_size),
@@ -570,22 +615,30 @@ class FocusIndicator:
         solid = foots.union(wall1).union(wall2).union(wall3).union(indicator)
         solid = solid.rotate((0, 0, 0), (0, -1, 0), sketch_angle_deg)
 
-        solid = solid.cut(ref.solid)
+        solid = solid.cut(
+            body.bounding_solid.translate((-GENERIC_GAP, 0, -GENERIC_GAP))
+        )
 
         self.solid = solid
         self.angle_deg = sketch_angle_deg
 
 
-ref = Reflector()
-cap = Cap()
-arm_holder = ArmHolder()
-focus_indicator = FocusIndicator(ref)
+mirror_segment = MirrorSegment()
+mirror_fastener = MirrorFastener()
+led_arm_base = LedArmBase()
+focus_indicator = FocusIndicator(mirror_segment)
 
-offset = 0
-show_object(ref.solid, options={"color": "#0f0"})
-show_object(cap.solid.translate((0, 0, offset)), options={"color": "#08f"})
-show_object(arm_holder.solid, options={"color": "#f80"})
-show_object(focus_indicator.solid, options={"color": "#84f"})
+preview_offset = 10
+reflector_solid = mirror_segment.reflector_solid.translate((0, 0, preview_offset))
+frame_solid = mirror_segment.frame_solid.translate((0, 0, preview_offset * 2))
+mirror_cap_solid = mirror_fastener.solid.translate((0, 0, preview_offset * 3))
+arm_holder_solid = led_arm_base.solid.translate((preview_offset, 0, preview_offset * 2))
+focus_indicator_solid = focus_indicator.solid
+show_object(reflector_solid, options={"color": "#eee"})
+show_object(frame_solid, options={"color": "#888"})
+show_object(mirror_cap_solid, options={"color": "#111"})
+show_object(arm_holder_solid, options={"color": "#111"})
+show_object(focus_indicator_solid, options={"color": "#84f"})
 
 # 焦点の位置 (参考用)
 focus_marker = cq.Workplane("XY").box(2, 2, 2)
@@ -593,13 +646,16 @@ show_object(focus_marker.translate(ELLIPSE_F_NEAR), options={"color": "#f00"})
 show_object(focus_marker.translate(ELLIPSE_F_FAR), options={"color": "#f00"})
 
 
-step = ref.solid.rotate((0, 0, 0), (0, 1, 0), 45)
-step.export(f"{STEP_OUT_DIR}/reflector.step")
+step = mirror_segment.reflector_solid.rotate((0, 0, 0), (0, -1, 0), -ELLIPSE_ANGLE)
+step.export(f"{STEP_OUT_DIR}/mirror_reflector.step")
 
-cap.solid.export(f"{STEP_OUT_DIR}/cap.step")
+step = mirror_segment.frame_solid.rotate((0, 0, 0), (0, -1, 0), 180)
+step.export(f"{STEP_OUT_DIR}/mirror_frame.step")
 
-step = arm_holder.solid.rotate((0, 0, 0), (0, 1, 0), -90)
-step.export(f"{STEP_OUT_DIR}/arm_holder.step")
+mirror_fastener.solid.export(f"{STEP_OUT_DIR}/mirror_fastener.step")
+
+step = led_arm_base.solid.rotate((0, 0, 0), (0, 1, 0), -90)
+step.export(f"{STEP_OUT_DIR}/led_arm_base.step")
 
 step = focus_indicator.solid.rotate(
     (0, 0, 0), (0, 1, 0), focus_indicator.angle_deg + 90
